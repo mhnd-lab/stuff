bc1d4f1d1c097f27e4e39dd90b0699047a397654
Muhannad Daoud <59766051+mhnd-lab@users.noreply.github.com>
2021-08-07 23:07:03 +0300

    Add files via upload
    my new i3 DebLoat stuff

--- debloat-i3-install.sh
+++ debloat-i3-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 apt update
 
-for package in i3 xorg xfe xpdf pulseaudio pavucontrol tilix vim git links nm-tray firefox-esr thunderbird libreoffice   
+for package in i3 i3blocks xorg xfe pcmanfm xpdf pulseaudio pavucontrol tilix vim git links nm-tray chromium firefox-esr thunderbird mpv   
 do
 apt install -qq --print-uris $package >> script.log 2>>script_error.log
 
 done
+
+cp config ~/.config/i3
+cp i3blocks.conf /etc
